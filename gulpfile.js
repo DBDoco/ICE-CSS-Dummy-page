c12253293738198ae59e21532bb2af4a943e2264
Dominik Bedenic <60035381+DBDoco@users.noreply.github.com>
2023-01-07 14:51:00 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
+
+
+function buildStyles() {
+    return src('ice/**/*.scss')
+        .pipe(sass())
+        .pipe(purgecss({ content: ['*.html'] }))
+        .pipe(dest('css'))
+}
+
+function watchTask() {
+    watch(['sass/**/*.scss', '*.html'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
